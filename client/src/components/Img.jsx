0589bac3bf9aaac8e68c07a94316f202c60d0355
JuanCMPDev <juancarlosmpdev@gmail.com>
2024-05-20 14:46:44 -0500

    Sixth commit: react slider done

--- client/src/components/Img.jsx
+++ client/src/components/Img.jsx
@@ -1,17 +1,16 @@
-  import React from 'react';
-
+  // eslint-disable-next-line react/prop-types
   const Img = ({ isLarge, img, caption }) => {
 
-    console.log("Caption:", caption); // Agregar este console.log para depurar
-
     if (isLarge) {
+      console.log('large' + caption);
       return (
-        <div className="flex flex-col justify-center items-center">
-          <h1>{caption}</h1>
-          <img src={img} alt={caption} className="w-[700px] h-[500px]" />
+        <div className="flex flex-col justify-center items-center my-7">
+          <h1 className="text-3xl pb-5 font-bold">{caption}</h1>
+          <img src={img} alt={caption} className="w-[700px] h-[400px]" />
         </div>
       );
     } else {
+      console.log('small '+caption);
       return <img src={img} alt={caption}/>;
     }
   }
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import Img from './Img';
 
@@ -7,13 +6,23 @@ import img2 from '../img/img2.jpg';
 import img3 from '../img/img3.webp';
 import img4 from '../img/img4.jpg';
 import img5 from '../img/img5.jpg';
+import img6 from '../img/img6.jpg';
+import img7 from '../img/img7.jpg';
+import img8 from '../img/img8.jpg';
+import img9 from '../img/img9.jpg';
+import img10 from '../img/img10.jpg';
 
 const elements = [
   { img: img1, caption: 'Caño cristales' },
   { img: img2, caption: 'Cerros de Mavecure' },
   { img: img3, caption: 'Puerto Nariño' },
   { img: img4, caption: 'Raudal de jirijirimo' },
-  { img: img5, caption: 'Santuario de Las Lajas' }
+  { img: img5, caption: 'Santuario de Las Lajas' },
+  { img: img6, caption: 'Rio Arauca'},
+  { img: img7, caption: 'Rio Amazonas'},
+  { img: img8, caption: 'Parque de los nevados'},
+  { img: img9, caption: 'Cabo de la vela'},
+  { img: img10, caption: 'Bosque de santander'}
 ];
 
 const Navigation = () => {
@@ -22,10 +31,10 @@ const Navigation = () => {
       {elements.map((element, index) => (
         <Link key={index} to={`/img${index + 1}`} className='mr-4 flex-0-0-auto'>
           <figure>
-            <div className='w-[250px] h-[250px] overflow-hidden flex justify-center items-center'>
+            <div className='w-[250px] min-h-[150px] h-[150px] overflow-hidden flex justify-center items-center'>
               <Img isLarge={false} img={element.img} caption={element.caption}/>
             </div>
-            <figcaption>{element.caption}</figcaption>
+            <figcaption className='text-center'>{element.caption}</figcaption>
           </figure>
         </Link>
       ))}
